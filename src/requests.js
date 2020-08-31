2729029beb59216aa98b851628a2538cc5f2f849
unknown <dracsharp@gmail.com>
2020-08-31 15:24:43 +0100

    init

---
+++ src/requests.js
@@ -0,0 +1,7 @@
+const API_KEY = "b78f98cb968fbd75805f93dc033eec88";
+
+const requests = {
+    fetchTrending: `/trending/all/week/api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTrending: `/trending/all/week/api_key=${API_KEY}&language=en-US`,
+}
